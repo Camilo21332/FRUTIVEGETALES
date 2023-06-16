bcd15d856704cec6afa9e531242a621da36ca040
camilo <camiloooool12@gmail.com>
2023-06-16 16:50:09 -0500

    se agrego proyecto

---
+++ database/migrations/2023_06_16_201103_create_usuario_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('USUnombres');
+            $table->string('USUapellidos');
+            $table->integer('USUedad');
+            $table->integer('USUtelefono');
+            $table->string('USUemail');
+            $table->string('USUcontraseña');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
