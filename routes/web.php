f0f1701917dccbc996f96d0f5063c068a002726e
camilo <camiloooool12@gmail.com>
2023-06-21 17:34:41 -0500

    actualizacion

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('index', function () {
+    return view('index');
+});
+Route::get('cart', function () {
+    return view('cart');
+});
