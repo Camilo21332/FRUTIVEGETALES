4ad822bd6eaad1d257b01676143c73faf668a99c
leidy <leidyjohanasantacruzdaza03@gmail.com>
2023-06-27 14:47:19 -0500

    version 4

---
+++ resources/views/pagos/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.plantilla')
+
+@section('content')
+    
+    <h1>Pago</h1>
+
+<form action="{{ route('pagos.store') }}" method="POST">
+
+    @csrf
+  <label>
+   pago
+    <br>
+    <input type="text" name="PAGOfactura">
+</label>
+ 
+<button type="submit">Enviar formulario</button>
+</form>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\productosController;
 use App\Http\Controllers\ventasController;
 use App\Http\Controllers\mensajesController;
-
-
+use App\Http\Controllers\pagosController;
 use App\Models\administradores;
 
 /*
@@ -29,11 +28,19 @@ Route::get('administradores/create', [administradoresController::class, 'create'
 Route::get('ventas/create', [ventasController::class, 'create']);
 Route::get('mensajes/create', [mensajesController::class, 'create']);
 Route::get('abastecimientos/create', [AbastecimientosController::class, 'create']);
+Route::get('pagos/create', [pagosController::class, 'create']);
+
+
+
 Route::post('productos', [productosController::class, 'store'])->name('productos.store');
 Route::post('ventas', [ventasController::class, 'store'])->name('ventas.store');
 Route::post('administradores', [administradoresController::class, 'store'])->name('administradores.store');
 Route::post('mensajes', [mensajesController::class, 'store'])->name('mensajes.store');
 Route::post('abastecimientos', [AbastecimientosController::class, 'store'])->name('abastecimientos.store');
+Route::post('pagos', [pagosController::class, 'store'])->name('pagos.store');
+
+
+
 Route::get('index', function () {
     return view('index');
 });
